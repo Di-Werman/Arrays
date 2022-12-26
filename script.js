419b9f4bfc94e2384a5a887c369d21adaae43a0d
Di-Werman <118116167+Di-Werman@users.noreply.github.com>
2022-12-26 10:20:44 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+
+var phoneId = 1
+var phoneName = 'Samsung'
+var phonePrice = 500
+
+  if (phonePrice < 500) {
+    console.log (phoneName + ' дешевле 500$')
+  }
+
+var phoneId = 2
+var phoneName = 'Xiaomi'
+var phonePrice = 200
+
+  if (phonePrice < 500) {
+    console.log (phoneName + ' дешевле 500$')
+  }
+
+var phoneId = 3
+var phoneName = 'Oppo'
+var phonePrice = 250
+
+  if (phonePrice < 500) {
+    console.log (phoneName + ' дешевле 500$')
+  }
